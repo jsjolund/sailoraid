6a011981a3574381d14e0cef83e8dc7ac1b2c91f
Johannes Sjölund <j.sjolund@gmail.com>
2017-10-05 02:10:57 +0200

    Faster sensor logging in MATLAB

--- Utilities/matlab/main.m
+++ Utilities/matlab/main.m
@@ -1,58 +1,52 @@
-% Example program which plots from serial
+%% Script which logs sensor values from serial port and can plot real-time
+clear -global;
 clear;
 
 %serialPort = 'COM1'; % Windows
-serialPort = '/dev/ttyACM3'; % Linux
+serialPort = '/dev/ttyACM0'; % Linux
 
-global hp step sensorLog
-
-% % Create a figure to plot into
-% nPointsInFigure = 250;  % Number of "sliding points" in your figure
-% step = 0.01;         % X points spacing
-% xVals = linspace(-(nPointsInFigure-1)*step, 0, nPointsInFigure); % Prepare empty data for the plot
-% yVals = NaN(nPointsInFigure, 1);
-% fig = figure(1);
-% hp = plot(xVals , yVals); % Generate the plot (with empty data) it will be passed to the callback.
-% ylim([-3 3]);
-
-sensorLog = {};
+realTimePlot();
 
 % Start reading from serial
-try
-    serialRead(serialPort, @sensorUpdate);
-catch err
-    rethrow(err)
-end
+serialRead(serialPort, @sensorUpdateCallback);
 
-% fprintf('Logged %d samples\n', height(sensorLog));
-fprintf('Program ended...\n');
-    
-% This is run when sensor values are updated
-function sensorUpdate(sensor)
-%     fprintf('x=%f y=%f z=%f\n',sensor.imu.roll,sensor.imu.pitch,sensor.imu.yaw)
-%     plotUpdate(sensor);
-    logUpdate(sensor);
-end
-
-% % Update the plot
-% function plotUpdate(sensor)
-% global hp step
-% xVals = get(hp,'XData');
-% yVals = get(hp,'YData');
-% yVals = circshift(yVals,-1);
-% yVals(end) = sensor.imu.az;
-% xVals = circshift(xVals,-1);
-% xVals(end) = xVals(end-1) + step;
-% set(hp, 'XData', xVals, 'YData', yVals);
-% drawnow limitrate
-% end
-
-% Log the data
-function logUpdate(sensor)
+% Plot from sensor log
 global sensorLog
-[~,~,~,hours,minutes,seconds] = datevec(now);
-timeNow = 1000*(3600*hours + 60*minutes + seconds);
-row = {uint64(timeNow)};
-row{2}  = struct2cell(sensor);
-sensorLog = [sensorLog; row];
+imu = [sensorLog.imu];
+plot([sensorLog.timestamp],[imu.az])
+fprintf('Program terminated.\n');
+    
+%% This callback is run when sensor values are updated
+function sensorUpdateCallback(sensor)
+global hp sensorLog
+sensorLog = [sensorLog, sensor];
+if ishghandle(hp)
+    realTimePlotUpdate(sensor);
+end
+end
+
+%% Real time plot functions
+% Create a figure to plot into
+function realTimePlot()
+global hp step
+nPointsInFigure = 250;  % Number of "sliding points" in your figure
+step = 0.01;         % X points spacing
+xVals = linspace(-(nPointsInFigure-1)*step, 0, nPointsInFigure); % Prepare empty data for the plot
+yVals = NaN(nPointsInFigure, 1);
+figure(1);
+hp = plot(xVals , yVals); % Generate the plot (with empty data) it will be passed to the callback.
+ylim([-3 3]);
+end
+
+% Update the plot by circle shifting the values as needed
+function realTimePlotUpdate(sensor)
+global hp step
+xVals = get(hp,'XData');
+yVals = get(hp,'YData');
+yVals = circshift(yVals,-1);
+yVals(end) = sensor.imu.az; % Plot variable
+xVals = circshift(xVals,-1);
+xVals(end) = xVals(end-1) + step;
+set(hp, 'XData', xVals, 'YData', yVals);
+drawnow limitrate
 end
--- Utilities/matlab/serialRead.m
+++ Utilities/matlab/serialRead.m
@@ -1,5 +1,5 @@
 %% Start listening to sensor data
-function serialRead(serialPort,cbFunc)
+function serialRead(serialPort,callback)
 if exist('s', 'var')
     try
         fclose(s);
@@ -16,16 +16,17 @@ if (s.Status == 'closed')
     s.ByteOrder = 'littleEndian';
     s.BytesAvailableFcnMode = 'byte';
     s.BytesAvailableFcnCount = 30*4;
-    set(s,'BytesAvailableFcn',{@serialReceive,cbFunc})
+    set(s,'BytesAvailableFcn',{@serialReceive,callback})
     s.Timeout = 2;
     % Open the serial connection
-    fprintf('Opening connection.\n')
+    fprintf('Opening connection...\n')
     fopen(s);
+    pause(1);
     % Start the sensor value stream
-    fprintf(s,sprintf('\r\n\r\nmatlab\r\n'));
-    pause(2);
+    fprintf(s,sprintf('\r\n\r\n\r\nmatlab\r\n'));
+    pause(1);
     % Close the serial connection
-    fprintf('Closing connection.\n')
+    fprintf('Closing connection...\n')
     fclose(s);
     delete(s);
     clear s;
@@ -33,18 +34,17 @@ end
 end
 
 %% Read the serial stream
-function serialReceive(obj,~,cbFunc)
-% fprintf('receive\n');
+function serialReceive(obj,~,callback)
 while obj.BytesAvailable > 0 
     % Read data as float array
-    [sensordata,~] = fread(obj, 30*4, 'float32');
+    [data,~] = fread(obj, 30*4, 'float32');
     % If last float is not NaN, we need to sync the stream
-    if ~isnan(sensordata(30))
+    if ~isnan(data(30))
         fprintf('Synchronizing...\n');
         nanCnt = 0;
         while 1
-            [sensordata,~] = fread(obj, 1, 'uint8');
-            if sensordata(1) == 255
+            [data,~] = fread(obj, 1, 'uint8');
+            if data(1) == 255
                 nanCnt = nanCnt+1;
             else
                 nanCnt = 0;
@@ -56,36 +56,39 @@ while obj.BytesAvailable > 0
         return;
     end
     % Store in struct with fields
-    sensor.imu.ax = sensordata(1);
-    sensor.imu.ay = sensordata(2);
-    sensor.imu.az = sensordata(3);
-    sensor.imu.gx = sensordata(4);
-    sensor.imu.gy = sensordata(5);
-    sensor.imu.gz = sensordata(6);
-    sensor.imu.mx = sensordata(7);
-    sensor.imu.my = sensordata(8);
-    sensor.imu.mz = sensordata(9);
-    sensor.imu.roll = sensordata(10);
-    sensor.imu.pitch = sensordata(11);
-    sensor.imu.yaw = sensordata(12);
-    sensor.env.humidity = sensordata(13);
-    sensor.env.pressure = sensordata(14);
-    sensor.env.temperature = sensordata(15);
-    sensor.gps.time.year = typecast(sensordata(18),'int32');
-    sensor.gps.time.month = typecast(sensordata(17),'int32');
-    sensor.gps.time.day = typecast(sensordata(16),'int32');
-    sensor.gps.time.hour = typecast(sensordata(19),'int32');
-    sensor.gps.time.min = typecast(sensordata(20),'int32');
-    sensor.gps.time.sec = typecast(sensordata(21),'int32');
-    sensor.gps.time.hsec = typecast(sensordata(22),'int32');
-    sensor.gps.pos.latitude = sensordata(23);
-    sensor.gps.pos.longitude = sensordata(24);
-    sensor.gps.pos.elevation = sensordata(25);
-    sensor.gps.pos.speed = sensordata(26);
-    sensor.gps.pos.direction = sensordata(27);
-    sensor.gps.info.satUse = typecast(sensordata(28),'int32');
-    sensor.gps.info.satView = typecast(sensordata(29),'int32');
+    sensor.imu.ax = data(1);
+    sensor.imu.ay = data(2);
+    sensor.imu.az = data(3);
+    sensor.imu.gx = data(4);
+    sensor.imu.gy = data(5);
+    sensor.imu.gz = data(6);
+    sensor.imu.mx = data(7);
+    sensor.imu.my = data(8);
+    sensor.imu.mz = data(9);
+    sensor.imu.roll = data(10);
+    sensor.imu.pitch = data(11);
+    sensor.imu.yaw = data(12);
+    sensor.env.humidity = data(13);
+    sensor.env.pressure = data(14);
+    sensor.env.temperature = data(15);
+    sensor.gps.time.year = typecast(single(data(16)),'int32');
+    sensor.gps.time.month = typecast(single(data(17)),'int32');
+    sensor.gps.time.day = typecast(single(data(18)),'int32');
+    sensor.gps.time.hour = typecast(single(data(19)),'int32');
+    sensor.gps.time.min = typecast(single(data(20)),'int32');
+    sensor.gps.time.sec = typecast(single(data(21)),'int32');
+    sensor.gps.time.hsec = typecast(single(data(22)),'int32');
+    sensor.gps.pos.latitude = data(23);
+    sensor.gps.pos.longitude = data(24);
+    sensor.gps.pos.elevation = data(25);
+    sensor.gps.pos.speed = data(26);
+    sensor.gps.pos.direction = data(27);
+    sensor.gps.info.satUse = typecast(single(data(28)),'int32');
+    sensor.gps.info.satView = typecast(single(data(29)),'int32');
+    % Add a timestamp
+    [~,~,~,hours,minutes,seconds] = datevec(now);
+    sensor.timestamp = uint64(1000*(3600*hours + 60*minutes + seconds));
     % User callback
-    cbFunc(sensor);
+    callback(sensor);
 end
 end
